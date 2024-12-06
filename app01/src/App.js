04499df4556dec812bc955ffbf7b93c009fad213
kel-silva <kellogical1@gmail.com>
2024-12-06 19:01:18 -0300

    Aula-20

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,20 +1,27 @@
-
-import react from 'react'
-import Caixa from './componentes/Caixa'
-import Canal from './componentes/Canal'
-
+import react,{useState} from 'react'
 export default function App(){
 
+   const [nome,setNome]=useState();
+     const armazenar=(chave,valor)=>{
+
+        localStorage.setItem(chave,valor)
+     }
+
+     const consultar=(chave)=>{
+        alert (localStorage.getItem(chave))
+     }
+     const apagar=(chave)=>{
+
+        localStorage.removeItem(chave)
+     }
 return(
 <>
+<label>Digite seu nome</label><br/>
+<input type='text' value={nome} onChange={(e)=>setNome(e.target.value)}/><br/>
+<button onClick={()=>armazenar('ls_nome', nome)}>gravar nome</button>
+<button onClick={()=>consultar('ls_nome')}>ver nome</button>
+<button onClick={()=>apagar('ls_nome')}>remover nome</button>
 
-<Caixa site="www.cfbcursos.com.br">
-<h1>CFB cursos</h1>
-<p>Curso de React</p>
-
-
-</Caixa>
-<Canal/>
 </>
 )
 }
