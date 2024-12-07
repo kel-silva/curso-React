4a4cb83e30192d276ef73b21fa7d3852f006a775
kel-silva <kellogical1@gmail.com>
2024-12-07 13:23:26 -0300

    Aula-28

--- app01/src/App.js
+++ app01/src/App.js
@@ -1,36 +1,52 @@
-import React,{useState} from 'react'
-import Globais from './componentes/Globais'
-import Info from './componentes/Info'
+import React,{useState,useEffect} from 'react'
+import Pagina1 from './componentes/Pagina1'
+import Pagina2 from './componentes/Pagina2'
 
 export default function App(){
 
+    const [pagina,setPagina]=useState(0)
 
-const [resumo,setResumo]=useState(Globais.resumo)
 
-const gravarResmo=()=>{
+    useEffect(
 
-    Globais.resumo=resumo
+        ()=>{
+
+            const url =window.location.href
+            const res=url.split('?')
+            setPagina(res[1])
+        }
+    )
+
+    const linkPaginas=(p)=>{
+            if (p==1){
+
+            window.open('http://localhost:3000?1','_self')
+    }else if(p==2){
+
+        window.open('http://localhost:3000?2','_self')
+    }
 }
-const verResumo=()=>{
-    alert(Globais.resumo)
+
+    const retornarPagina=()=>{
+            if (pagina==1){
+
+                return <Pagina1/>
+
+            }else if (pagina==2){
+                return <Pagina2/>
+
+            }else{
+                    return <div>
+                               <button onClick={()=>linkPaginas(1)}>Pagina1</button>
+                                <button onClick={()=>linkPaginas(2)}>Pagina2</button>
+                         </div>
+    }
 }
 
 return(
 <>
-<hr/>
-<Info/> 
-<p>{'canal + ' + Globais.canal}</p>
-<p>{'cursos + ' + Globais.curso}</p>
-<p>{'Ano + ' + Globais.ano}</p>
-<hr/>
-
-<input type='text' value={resumo} onChange={(e)=>setResumo(e.target.value)} /> 
-<button onClick={()=>gravarResmo()}>Gravar Resumo</button>
-<button onClick={()=>verResumo()}>ver Resumo</button>
-
-
+{retornarPagina()}
 
 </>
-
 )
 }
---
+++ app01/src/componentes/Pagina1.js
@@ -0,0 +1,11 @@
+import react from 'react'
+export default function Pagina1(){
+
+return(
+<div>
+<h1>Pagina 01</h1>
+<h3>curso de react</h3>
+
+</div>
+)
+}
